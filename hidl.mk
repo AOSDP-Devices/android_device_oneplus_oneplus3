7dcfdfe3963a701b8d663aee74f2eedab3e62165
Miguel Martinez <slverwolf@gmail.com>
2018-09-05 13:04:20 -0500

    Import PowerHAL

--- hidl.mk
+++ hidl.mk
@@ -58,6 +58,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-service
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service.oneplus3
+
 # health
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
