0e6a8ff6a11f3b8a0439cb24a02964161dd944d4
Miguel Martinez <slverwolf@gmail.com>
2018-10-05 12:49:01 -0500

    Fix fingerprint not working on first try

--- device.mk
+++ device.mk
@@ -144,7 +144,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    fingerprint.msm8996
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # GPS
 PRODUCT_PACKAGES += \
